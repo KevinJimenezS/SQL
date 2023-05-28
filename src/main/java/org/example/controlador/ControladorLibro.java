657ac7dc5c3f61e2e687eef5b3cf9172268ff6b2
KevinJimenezS <125950028+KevinJimenezS@users.noreply.github.com>
2023-05-27 22:30:35 -0600

    Update ControladorLibro.java

--- src/main/java/org/example/controlador/ControladorLibro.java
+++ src/main/java/org/example/controlador/ControladorLibro.java
@@ -1,8 +1,10 @@
 package org.example.controlador;
 
+import org.example.modelo.Libro;
 import org.example.modelo.ModeloTablaLibro;
 import org.example.vista.VentanaLibro;
 
+import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -10,18 +12,35 @@ public class ControladorLibro extends MouseAdapter {
 
     private VentanaLibro view;
     private ModeloTablaLibro modelo;
-    public ControladorLibro(VentanaLibro view){
+
+    public ControladorLibro(VentanaLibro view) {
         this.view = view;
+        modelo = new ModeloTablaLibro();
+        this.view.getTblLibro().setModel(modelo);
         this.view.getBtnCargar().addMouseListener(this);
+        this.view.getBtnAgregar().addMouseListener(this);
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        if (e.getSource() == this.view.getBtnCargar()){
-            modelo = new ModeloTablaLibro();
+        if (e.getSource() == this.view.getBtnCargar()) {
+
             modelo.cargarDatos();
             this.view.getTblLibro().setModel(modelo);
             this.view.getTblLibro().updateUI();
+        }
+        if (e.getSource() == this.view.getBtnAgregar()) {
+            Libro libro = new Libro();
+            libro.setId(0);
+            libro.setTitulo(this.view.getTxtTitulo().getText());
+            libro.setAutor(this.view.getTxtAutor().getText());
+            if (modelo.agregarLibro(libro)) {
+                JOptionPane.showMessageDialog(view, "SE AGREGO CORRECTAMENTE", "AVISO", JOptionPane.INFORMATION_MESSAGE);
+                this.view.getTblLibro().updateUI();
+            } else {
+                JOptionPane.showMessageDialog(view, "NO SE PUDO AGREGAR, REVISAR CONEXION", "ERROR INTO", JOptionPane.ERROR_MESSAGE);
+            }
+            this.view.limpiar();
         }
     }
 }
